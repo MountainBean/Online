2824e21c44bd0ee78c0e62563fb14613506ecc75
Samuel Drew <samueld.work@gmail.com>
2024-07-14 23:18:35 +1000

    added models for storing photos and basic index view

--- Photos/models.py
+++ Photos/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Photographer(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return self.full_name()
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=256)
+    location = models.CharField(max_length=80)
+    file = models.CharField(max_length=256, null=True)
+    photographer = models.ForeignKey(
+        Photographer, on_delete=models.CASCADE, null=True, related_name="photos")
+    date = models.DateField()
+    slug = models.SlugField(default="", null=False)
+
+    def __str__(self):
+        return f"{self.title} - {self.photographer}, {self.date}"
--- Photos/views.py
+++ Photos/views.py
@@ -1,3 +1,13 @@
+from datetime import date
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Photographer, Photo
+
+
+def index(request):
+    uploaded_photos = Photo.objects.all().order_by("title")
+    num_photos = uploaded_photos.count()
+    return render(request, "photos/index.html", {
+        "uploaded_photos": uploaded_photos,
+        "total_number_of_uploaded_photos": num_photos
+    })
