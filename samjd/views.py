bf5ebe30e1a888796dc474429ca391638c934ec3
Samuel Drew <samueld.work@gmail.com>
2024-08-14 17:10:16 +1000

    limit calling rss from blog to public ip addresses (stop EB health checker from spamming hasnode's API and getting me throttled)

--- samjd/views.py
+++ samjd/views.py
@@ -1,13 +1,18 @@
 from datetime import date
 from django.shortcuts import render, get_object_or_404
 from feedparser import parse
+from ipware import get_client_ip
 
 
 def starting_page(request):
-    print("parsing for blog posts")
-    feed = parse("https://blog.mountainbean.online/rss.xml")
-    print(f"{feed.status=}")
-    top_five = feed.entries[:5]
+    top_five = None
+    ip, isroutable = get_client_ip(request)
+    print(f"new request from {ip=}. {isroutable=}")
+    if isroutable or ip == "127.0.0.1":
+        print("parsing for blog posts")
+        feed = parse("https://blog.mountainbean.online/rss.xml")
+        print(f"{feed.status=}")
+        top_five = feed.entries[:5]
     return render(request, "samjd/index.html", {
         "entries": top_five
     })
