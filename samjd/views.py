fc86a7edd9b7b271378700b04c216702f2c6d0a4
Samuel Drew <samueld.work@gmail.com>
2024-08-14 14:45:27 +1000

    added rss parser status code debug line

--- samjd/views.py
+++ samjd/views.py
@@ -6,6 +6,7 @@ from feedparser import parse
 def starting_page(request):
     print("parsing for blog posts")
     feed = parse("https://blog.mountainbean.online/rss.xml")
+    print(f"{feed.status=}")
     top_five = feed.entries[:5]
     return render(request, "samjd/index.html", {
         "entries": top_five
