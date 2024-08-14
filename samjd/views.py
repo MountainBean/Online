00a417fd20f3166ef1fcbc2c6681406ec8b19ece
Samuel Drew <samueld.work@gmail.com>
2024-08-14 21:25:14 +1000

    replaced rss feed parser with gql api call

--- samjd/views.py
+++ samjd/views.py
@@ -1,7 +1,24 @@
 from datetime import date
-from django.shortcuts import render, get_object_or_404
-from feedparser import parse
+from django.shortcuts import render
 from ipware import get_client_ip
+import requests
+import json
+
+blog_query = '''
+query Publication {
+    publication(host: "blog.mountainbean.online") {
+        posts(first: 5) {
+            edges {
+                node {
+                    title
+                    brief
+                    url
+                }
+            }
+        }
+    }
+}
+'''
 
 
 def starting_page(request):
@@ -9,9 +26,12 @@ def starting_page(request):
     ip, isroutable = get_client_ip(request)
     if isroutable or ip == "127.0.0.1":
         print("parsing for blog posts")
-        feed = parse("https://blog.mountainbean.online/rss.xml")
-        print(f"{feed.status=}")
-        top_five = feed.entries[:5]
+        blog_response = requests.post(
+            url="https://gql.hashnode.com",
+            json={"query": blog_query})
+        five_latest = json.loads(
+            blog_response.content
+        )['data']['publication']['posts']['edges']
     return render(request, "samjd/index.html", {
-        "entries": top_five
+        "entries": five_latest
     })
