32ef4f87cef5a8cad8166c68961c384ffe4012d9
Samuel Drew <samueld.work@gmail.com>
2024-08-30 22:48:16 +1000

    complete user data

--- .ec2config/userdata.sh
+++ .ec2config/userdata.sh
@@ -3,6 +3,7 @@
 METADATATOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 INTERFACE_MAC=`curl http://169.254.169.254/latest/meta-data/mac -H "X-aws-ec2-metadata-token: $METADATATOKEN"`
 PUBLIC_IP=`curl http://169.254.169.254/latest/meta-data/network/interfaces/macs/$INTERFACE_MAC/public-ipv4s -H "X-aws-ec2-metadata-token: $METADATATOKEN"`
+DJ_SECRET_KEY=
 
 cat <<EOT >> change_batch.json
 {
@@ -18,6 +19,8 @@ cat <<EOT >> change_batch.json
 }
 EOT
 aws route53 change-resource-record-sets --hosted-zone-id Z08242151MWOY90HMWF69 --change-batch file://change_batch.json
+
+sudo groupadd www-data
 
 aws s3api get-object\
  --bucket website-source-artifacts-manual-in-console\
@@ -35,15 +38,46 @@ python3.11 -m venv .venv
 . ./.venv/bin/activate
 
 python3.11 -m pip install -r requirements.txt
+deactivate
 
-sudo cp .ec2config/gunicorn.{socket,service} /etc/systemd/system/
-sudo cp .ec2config/ngix.conf /etc/nginx/conf.d/Online.conf
+cat <<EOT >> gunicorn.service
+[Unit]
+Description=gunicorn daemon
+Requires=gunicorn.socket
+After=network.target
+
+[Service]
+User=ec2-user
+Group=www-data
+WorkingDirectory=/home/ec2-user/Website
+ExecStart=/home/ec2-user/Website/.venv/bin/gunicorn \
+          --access-logfile - \
+          --workers 1 \
+          --threads 15 \
+          --bind unix:/run/gunicorn.sock \
+          Online.wsgi:application \
+          --env DJANGO_WEBSITE_ENVIRONMENT=PROD \
+          --env DJ_SECRET_KEY=$DJ_SECRET_KEY
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+cat <<EOT >> gunicorn.socket
+[Unit]
+Description=gunicorn socket
+
+[Socket]
+ListenStream=/run/gunicorn.sock
+
+[Install]
+WantedBy=sockets.target
+EOT
+
+sudo cp gunicorn.{socket,service} /etc/systemd/system/
+sudo cp .ec2config/nginx.conf /etc/nginx/conf.d/Online.conf
 
 
 sudo systemctl start gunicorn.socket
 sudo systemctl enable gunicorn.socket
-
-
-
-
-
+sudo systemctl restart nginx
