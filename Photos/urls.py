d946d1e012c8a64a7e4c2dd426820dab7a70b811
Sam J Drew <101438022+MountainBean@users.noreply.github.com>
2024-12-08 16:41:07 +1300

    Delete capital P Photos directory

--- Photos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("all_photos", views.all_photos, name="all-photos-page"),
-    path("all_photos/<slug:slug>", views.photo_detail, name="photo-detail-page")
-]
